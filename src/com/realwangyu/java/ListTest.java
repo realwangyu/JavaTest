76a6422e824ee791a1551c1d0758962b567e9fa3
wangyu08334 <wangyu08334@hundsun.com>
2019-05-31 14:46:51 +0800

    List加对象测试

---
+++ src/com/realwangyu/java/ListTest.java
@@ -0,0 +1,52 @@
+/**     
+  * 文件描述
+  * @ProductName    Hundsun HEP
+  * @ProjectName    JavaTest
+  * @Package        com.realwangyu.java
+  * @Description    note
+  * @Author         wangyu08334
+  * @CreateDate     May 31, 2019 2:36:46 PM
+  * @UpdateRemark   The modified content
+  * @Version        1.0
+  *
+  * Copyright © 2019 Hundsun Technologies Inc. All Rights Reserved
+**/
+
+package com.realwangyu.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description    note
+ * @Author         wangyu08334
+ * @CreateDate     May 31, 2019 2:36:46 PM
+ */
+public class ListTest {
+
+    /**
+     * @Description    note
+     * @Author         wangyu08334
+     * @CreateDate     May 31, 2019 2:36:46 PM
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        List<TestObj> testObjs = new ArrayList<TestObj>();
+        TestObj testObj = new TestObj();
+        testObj.setI(1);
+        testObj.setS("one");
+        testObjs.add(testObj);
+        for (TestObj testObj1 : testObjs) {
+            System.out.println(testObj1.getI() + testObj1.getS());
+            System.out.println("1end");
+        }
+        testObj.setI(2);
+        testObj.setS("two");
+        testObjs.add(testObj);
+        for (TestObj testObj2 : testObjs) {
+            System.out.println(testObj2.getI() + testObj2.getS());
+        }
+    }
+
+}
---
+++ src/com/realwangyu/java/TestObj.java
@@ -0,0 +1,58 @@
+/**     
+  * 文件描述
+  * @ProductName    Hundsun HEP
+  * @ProjectName    JavaTest
+  * @Package        com.realwangyu.java
+  * @Description    note
+  * @Author         wangyu08334
+  * @CreateDate     May 31, 2019 2:37:57 PM
+  * @UpdateRemark   The modified content
+  * @Version        1.0
+  *
+  * Copyright © 2019 Hundsun Technologies Inc. All Rights Reserved
+**/
+
+package com.realwangyu.java;
+
+/**
+ * @Description    note
+ * @Author         wangyu08334
+ * @CreateDate     May 31, 2019 2:37:57 PM
+ */
+public class TestObj {
+    
+    private int i;
+    
+    private String s;
+
+    /**
+     * @return the i
+     */
+    public int getI() {
+        return i;
+    }
+
+    /**
+     * @param i the i to set
+     */
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    /**
+     * @return the s
+     */
+    public String getS() {
+        return s;
+    }
+
+    /**
+     * @param s the s to set
+     */
+    public void setS(String s) {
+        this.s = s;
+    }
+    
+    
+
+}
